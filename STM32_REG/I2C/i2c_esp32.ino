96487dc8af1a29335848601768ad56664b90cf90
Preethi D <27preethid@gmail.com>
2025-08-12 07:59:55 +0530

    STM32_REG

---
+++ STM32_REG/I2C/i2c_esp32.ino
@@ -0,0 +1,42 @@
+
+#include <Wire.h>
+#include<stdio.h>
+#define MY_ADDR   0x68
+#define SDA_PIN 21
+#define SCL_PIN 22
+
+int LED = 2;
+char rx_buffer[32] ;
+uint32_t cnt =0;
+//uint8_t message[50];
+
+
+void setup() {
+
+  Serial.begin(9600);
+  delay(500);
+
+  pinMode (LED, OUTPUT);
+  digitalWrite(2,HIGH);
+  Wire.begin((uint8_t)MY_ADDR, SDA_PIN, SCL_PIN);
+  Wire.onReceive(receiveEvent);
+  Serial.println("Waiting for data from master");  
+}
+
+void loop(void)
+{
+ 
+}
+
+void receiveEvent(int bytes) 
+{
+  digitalWrite(2,LOW);
+ while( Wire.available() )
+ {
+   rx_buffer[cnt++] = Wire.read();
+ }
+  rx_buffer[cnt] = '\0';
+  cnt=0;
+  Serial.print("Received:");  
+  Serial.println((char*)rx_buffer);  
+}
---
+++ STM32_REG/I2C/main.c
@@ -0,0 +1,122 @@
+#include<stdio.h>
+#include<string.h>
+#include "stm32l4xx.h"
+#include"stm32l4xx_i2c_driver.h"
+#include"stm32l4xx_usart_driver.h"
+
+#define SLAVE_ADDR  0x68
+#define MY_ADDR 0x61;
+
+I2C_Handle_t I2C1Handle;
+
+uint8_t some_data[] = "I2C";
+
+
+void I2C1_GPIOInits(void)
+{
+	GPIO_Handle_t I2CPins;
+
+	I2CPins.pGPIOx = GPIOB;
+	I2CPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	I2CPins.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	I2CPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+	I2CPins.GPIO_PinConfig.GPIO_PinAltFunMode = 4;
+	I2CPins. GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO_PeriClockControl(GPIOB, ENABLE);
+	//scl
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_6;
+	GPIO_Init(&I2CPins);
+
+
+	//sda
+	I2CPins.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_7;
+
+	GPIO_Init(&I2CPins);
+
+
+}
+
+void I2C1_Inits(void)
+{
+	I2C1Handle.pI2Cx = I2C1;
+	I2C1Handle.I2C_Config.I2C_ACKControl = I2C_ACK_ENABLE;
+	I2C1Handle.I2C_Config.I2C_DeviceAddress = MY_ADDR;
+
+	I2C_Init(&I2C1Handle);
+
+}
+
+void GPIO_ButtonInit(void)
+{
+	GPIO_Handle_t GPIOBtn;
+
+	GPIOBtn.pGPIOx = GPIOC;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_13;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+	GPIO_PeriClockControl(GPIOC, ENABLE);
+	GPIO_Init(&GPIOBtn);
+
+}
+
+uint8_t msg[50]="Transmit completed\r\n";
+
+USART_Handle_t usart2_handle;
+void delay(void)
+{
+	for(uint32_t i = 0 ; i < 250000 ; i ++);
+}
+void USART2_Init(void)
+{
+
+	usart2_handle.pUSARTx =USART2;
+	usart2_handle.USART_Config.USART_Baud=USART_STD_BAUD_115200;
+	usart2_handle.USART_Config.USART_HWFlowControl=USART_HW_FLOW_CTRL_NONE;
+	usart2_handle.USART_Config.USART_Mode=USART_MODE_TXRX;
+	usart2_handle.USART_Config.USART_NoOfStopBits=USART_STOPBITS_1;
+	usart2_handle.USART_Config.USART_WordLength=USART_WORDLEN_8BITS;
+	usart2_handle.USART_Config.USART_ParityControl=USART_PARITY_DISABLE;
+	USART_Init(&usart2_handle);
+}
+
+
+void USART2_GPIOInit(void)
+{
+	GPIO_Handle_t usart2_gpio;
+	memset(&usart2_gpio,0,sizeof(usart2_gpio));
+	usart2_gpio.pGPIOx =GPIOA;
+	usart2_gpio.GPIO_PinConfig.GPIO_PinMode=GPIO_MODE_ALTFN;
+	usart2_gpio.GPIO_PinConfig.GPIO_PinOPType=GPIO_OP_TYPE_PP;
+	usart2_gpio.GPIO_PinConfig.GPIO_PinPuPdControl=GPIO_PIN_PU;
+	usart2_gpio.GPIO_PinConfig.GPIO_PinSpeed=GPIO_SPEED_FAST;
+	usart2_gpio.GPIO_PinConfig.GPIO_PinAltFunMode=7;
+
+	usart2_gpio.GPIO_PinConfig.GPIO_PinNumber=GPIO_PIN_NO_2;
+	GPIOA_PCLK_EN();
+	GPIO_Init(&usart2_gpio);
+
+	usart2_gpio.GPIO_PinConfig.GPIO_PinNumber=GPIO_PIN_NO_3;
+	GPIO_Init(&usart2_gpio);
+}
+int main(void)
+{
+
+	USART2_GPIOInit();
+	USART2_Init();
+	USART_PeripheralControl(USART2,ENABLE);
+	GPIO_ButtonInit();
+	I2C_PeriClockControl(I2C1, ENABLE);
+	I2C1_GPIOInits();
+	I2C1_Inits();
+	I2C_PeripheralControl(I2C1,ENABLE);
+
+	while(1)
+	{
+		while(  GPIO_ReadFromInputPin(GPIOC,GPIO_PIN_NO_13)==1 );
+		delay();
+		I2C_MasterSendData(&I2C1Handle,some_data,3,SLAVE_ADDR,0);
+		USART_SendData(&usart2_handle, msg,20);
+	}
+
+}
